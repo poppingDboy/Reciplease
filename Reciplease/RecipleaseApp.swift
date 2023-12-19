07a3f0c87c494a7c6d8314036d59d5b1e3e56393
Dylan Marteau <dylan_marteau@connect-tech.sncf>
2023-12-19 23:26:00 +0100

    Initial Commit

---
+++ Reciplease/RecipleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by MARTEAU Dylan on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
